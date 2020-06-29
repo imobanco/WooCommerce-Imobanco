6e0391e2cd320b4818daa4b751c7722327bfd1db
Mateus Sátiro <mateus_sat@hotmail.com>
2020-06-29 14:11:51 -0300

    Feature/wp gateway plugin (#6)
    * Cria a estrutura de folder do plugin
    
    * Cria um arquivo .php com o mesmo nome do folder e renoma a brach
    
    * Adiciona nome do plugin, descrição e author
    
    * Adiciona o volume aonde vai ficar o plugin da wp-zoop-gateway
    
    * Cria a estrutura de pastas
    
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>

---
+++ wp-zoop-gateway/wp-zoop-gateway.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+Plugin Name: wp-zoop-gateway
+Plugin URI: https://www.imobanco.com.br/
+Description: Plugin para utilizar o gateway de pagamento do Imobanco
+Version: 0.0.1
+Author: Imobanco
+Author URI: https://www.imobanco.com.br/
+License: GPLv2 or later
+
+*/
