db17ab72541c29040cc97bf94ef8745ee87441e1
Mateus Sátiro <mateus_sat@hotmail.com>
2020-06-29 11:54:21 -0300

    Update wp-zoop-gateway/wp-zoop-gateway.php
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>

--- wp-zoop-gateway/wp-zoop-gateway.php
+++ wp-zoop-gateway/wp-zoop-gateway.php
@@ -5,7 +5,7 @@
 Plugin Name: wp-zoop-gateway
 Plugin URI: https://www.imobanco.com.br/
 Description: Plugin para utilizar o gateway de pagamento do Imobanco
-Version: 1.0.0
+Version: 0.0.1
 Author: Imobanco
 Author URI: https://www.imobanco.com.br/
 License: GPLv2 or later
