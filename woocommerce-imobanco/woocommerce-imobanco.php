0691edb7c64aab07ef65f454a453ff7c73e0ced6
Mateus <mateus_sat@hotmail.com>
2020-07-06 18:09:00 -0300

    Exemplo de uma chamada a API

--- woocommerce-imobanco/woocommerce-imobanco.php
+++ woocommerce-imobanco/woocommerce-imobanco.php
@@ -12,23 +12,70 @@ License: GPLv2 or later
 
 */
 
-/** Step 2 (from text above). */
-add_action( 'admin_menu', 'my_plugin_menu' );
-
 /** Step 1. */
-function my_plugin_menu() {
-	add_options_page( 'My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-}
+// function my_plugin_menu() {
+// 	add_options_page( 'My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+// }
+
+/** Step 2 (from text above). */
+// add_action( 'admin_menu', 'my_plugin_menu' );
 
 /** Step 3. */
-function my_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-        wp_die( __( 'Você não tem permissão suficiente para acessar essa pagina' ) );        
-	}
+// function my_plugin_options() {
+// 	if ( !current_user_can( 'manage_options' ) )  {
+//         wp_die( __( 'Você não tem permissão suficiente para acessar essa pagina' ) );        
+// 	}
 
-	$url = 'http://django:8000/transactions/create_invoice_transaction/';
+	// $url = 'http://django:8000/transactions/create_invoice_transaction/';
 
 	 
+
+	// $response = wp_remote_post($url,$args = [
+
+	// 	'headers' =>[
+	// 		'Content-Type' => 'application/json',
+	// 		'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
+	// 		]
+	// 	,
+	// 	'body' => json_encode([ 
+	// 		'amount' => 500,
+	// 		'description' => 'transação de cartão',
+	// 		'payer' => 'cadc937e-2ac4-4085-be83-3a5190376b80',
+	// 		'receiver' => '46fe7215-5983-4d38-b400-1b14fe50d9e0',
+	// 		'payment_method' => [			
+	// 			'expiration_date' => '2020-06-25',
+	// 			'limit_date' => '2020-07-14'
+	// 		]			
+	// 	])
+	// ]
+			
+	// );
+	
+	// $http_code = wp_remote_retrieve_response_code( $response );
+		
+	//  $body = json_decode($response['body'],true);
+	//  echo '<pre>';
+	//  print_r($body['payment_method']['id']);
+	//  echo '</pre>';
+// }
+
+// essa função cria um paragrafo na tela principal do WP
+function ola_plugin(){
+	
+	?>
+	<div>
+		<p>Teste de Atualização!<p>
+	<div>
+	<?php
+}
+
+// add_action('all_admin_notices', 'ola_plugin');
+
+add_action( 'profile_update', 'chama_api', 10, 2 );
+
+function chama_api($current_user) {
+	
+	$url = 'http://django:8000/transactions/create_invoice_transaction/';
 
 	$response = wp_remote_post($url,$args = [
 
@@ -51,15 +98,14 @@ function my_plugin_options() {
 			
 	);
 	
-	$http_code = wp_remote_retrieve_response_code( $response );
+	// $http_code = wp_remote_retrieve_response_code( $response );
 		
-	$body = json_decode($response['body'],true);
-	echo '<pre>';
-	var_dump($body['payment_method']['id']);
-	echo '</pre>';
+	//  $body = json_decode($response['body'],true);
+	//  echo '<pre>';
+	//  print_r($body['payment_method']['id']);
+	//  echo '</pre>';
+	
+	
 }
+
 ?>
-
-
-
-
