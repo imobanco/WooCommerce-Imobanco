b0c70c492ebab01475910efbe06b66f9e77441fa
Mateus <mateus_sat@hotmail.com>
2020-07-08 11:55:54 -0300

    Modifica a chamada para buyer

--- woocommerce-imobanco/woocommerce-imobanco.php
+++ woocommerce-imobanco/woocommerce-imobanco.php
@@ -13,48 +13,47 @@ License: GPLv2 or later
 */
 
 /** Step 1. */
-//  function my_plugin_menu() {
-//  	add_options_page( 'My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-//  }
+  function my_plugin_menu() {
+  	add_options_page( 'My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+  }
 
 /** Step 2 (from text above). */
-//  add_action( 'admin_menu', 'my_plugin_menu' );
+  add_action( 'admin_menu', 'my_plugin_menu' );
 
 /** Step 3. */
-//  function my_plugin_options($current_use) {
-//  	if ( !current_user_can( 'manage_options' ) )  {
-//          wp_die( __( 'Você não tem permissão suficiente para acessar essa pagina' ) );        
-//  	}
+  function my_plugin_options($current_use) {
+  	if ( !current_user_can( 'manage_options' ) )  {
+         wp_die( __( 'Você não tem permissão suficiente para acessar essa pagina' ) );        
+  	}
 
-// 	 $url = 'http://django:8000/transactions/create_invoice_transaction/';	 
+ 	 $url = 'http://django:8000/buyers/';	 
 
-// 	 $response = wp_remote_post($url,$args = [
+ 	 $response = wp_remote_post($url,$args = [
 
-// 	 	'headers' =>[
-// 	 		'Content-Type' => 'application/json',
-// 	 		'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
-// 	 		]
-// 	 	,
-// 	 	'body' => json_encode([ 
-// 	 		'amount' => 500,
-// 	 		'description' => 'transação de cartão',
-// 	 		'payer' => 'cadc937e-2ac4-4085-be83-3a5190376b80',
-// 	 		'receiver' => '46fe7215-5983-4d38-b400-1b14fe50d9e0',
-// 	 		'payment_method' => [			
-// 	 			'expiration_date' => '2020-06-25',
-// 	 			'limit_date' => '2020-07-14'
-// 	 		]			
-// 	 	])
-// 	 ]
+ 	 	'headers' =>[
+	 		'Content-Type' => 'application/json',
+ 	 		'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
+ 	 		]
+ 	 	,
+ 	 	'body' => json_encode([ 
+ 	 		'birthdate' => '2000-10-10',
+ 	 		'cpf_cnpj' => '18236120000158',
+ 	 		'email' => 'exemplo@exemplo.com',
+              'first_name' => 'Exemplo',
+              'last_name' => 'exemplo',
+              'mobile_phone' => '84999999999'
+ 	 				
+	 	])
+ 	 ]
 			
-// 	 );
+ 	 );
 	
-// 	 $http_code = wp_remote_retrieve_response_code( $response );		
-// 	  $body = json_decode($response['body'],true);
-// 	  echo '<pre>';
-// 	  print_r(var_dump($current_user->ID));
-// 	  echo '</pre>';
-//  }
+ 	 $http_code = wp_remote_retrieve_response_code( $response );		
+ 	  $body = json_decode($response['body'],true);
+ 	  echo '<pre>';
+ 	  print_r($body);
+ 	  echo '</pre>';
+  }
 
 // essa função cria um paragrafo na tela principal do WP
 /*function ola_plugin(){	
@@ -66,6 +65,8 @@ License: GPLv2 or later
 }*/
 
 // add_action('all_admin_notices', 'ola_plugin');
+
+/* Trecho de codigo não debugado */
 
 add_action( 'profile_update', 'chama_api', 10, 2 );
 
