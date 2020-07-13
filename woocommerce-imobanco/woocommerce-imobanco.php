473c1a046b72e06ffa710c1ae2bc020c5d7ddb11
Mateus <mateus_sat@hotmail.com>
2020-07-13 12:19:59 -0300

    Adiciona o campo de Telefone

--- woocommerce-imobanco/woocommerce-imobanco.php
+++ woocommerce-imobanco/woocommerce-imobanco.php
@@ -5,7 +5,7 @@
 Plugin Name: WooCommerce Imobanco
 Plugin URI: https://www.imobanco.com.br/
 Description: Plugin para utilizar o gateway de pagamento do Imobanco
-Version: 0.0.1
+Version: 0.0.2
 Author: Imobanco
 Author URI: https://www.imobanco.com.br/
 License: GPLv2 or later
@@ -295,5 +295,86 @@ add_action(
     'edit_user_profile_update',
     'wporg_usermeta_form_field_cpf_cnpj_update'
 );
+
+
+/**
+ * The field on the editing screens.
+ *
+ * @param $user WP_User user object
+ */
+
+function wporg_usermeta_form_field_phone( $user )
+{
+    ?>
+    <h3>It's Your Phone</h3>
+    <table class="form-table">
+        <tr>
+            <th>
+                <label for="phone">Phone Number</label>
+            </th>
+            <td>
+                <input type="number"
+                       class="regular-text ltr"
+                       id="phone"
+                       name="phone"
+                       value="<?= esc_attr( get_user_meta( $user->ID, 'phone', true ) ) ?>"
+                       title="Please use only numbers."                       
+                       required>
+                <p class="description">
+                    Please enter your phone without any caracter special.
+                </p>
+            </td>
+        </tr>
+    </table>
+    <?php
+}
+
+/**
+ * The save action.
+ *
+ * @param $user_id int the ID of the current user.
+ *
+ * @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
+ */
+
+function wporg_usermeta_form_field_phone_update( $user_id )
+{
+    // check that the current user have the capability to edit the $user_id
+    if ( ! current_user_can( 'edit_user', $user_id ) ) {
+        return false;
+    }
+  
+    // create/update user meta for the $user_id
+    return update_user_meta(
+        $user_id,
+        'phone',
+        $_POST['phone']
+    );
+}
+
+// Add the field to user's own profile editing screen.
+add_action(
+    'show_user_profile',
+    'wporg_usermeta_form_field_phone'
+);
+  
+// Add the field to user profile editing screen.
+add_action(
+    'edit_user_profile',
+    'wporg_usermeta_form_field_phone'
+);
+  
+// Add the save action to user's own profile editing screen update.
+add_action(
+    'personal_options_update',
+    'wporg_usermeta_form_field_phone_update'
+);
+
+// Add the save action to user profile editing screen update.
+add_action(
+    'edit_user_profile_update',
+    'wporg_usermeta_form_field_phone_update'
+);
+
 
 ?>
