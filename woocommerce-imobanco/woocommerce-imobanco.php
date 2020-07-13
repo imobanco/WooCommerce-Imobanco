777559ac0569f054275e66f947b4aee89dce774e
Mateus Sátiro <mateus_sat@hotmail.com>
2020-07-13 14:27:06 -0300

    Update woocommerce-imobanco/woocommerce-imobanco.php
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>

--- woocommerce-imobanco/woocommerce-imobanco.php
+++ woocommerce-imobanco/woocommerce-imobanco.php
@@ -179,7 +179,7 @@ function wporg_usermeta_form_field_birthday( $user ){
  * @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
  */
 
-function wporg_usermeta_form_field_birthday_update( $user_id )
+function wporg_usermeta_form_field_birthdate_update( $user_id )
 {
     // check that the current user have the capability to edit the $user_id
     if ( ! current_user_can( 'edit_user', $user_id ) ) {
@@ -189,33 +189,33 @@ function wporg_usermeta_form_field_birthday_update( $user_id )
     // create/update user meta for the $user_id
     return update_user_meta(
         $user_id,
-        'birthday',
-        $_POST['birthday']
+        'birthdate',
+        $_POST['birthdate']
     );
 }
 
 // Add the field to user's own profile editing screen.
 add_action(
     'show_user_profile',
-    'wporg_usermeta_form_field_birthday'
+    'wporg_usermeta_form_field_birthdate'
 );
   
 // Add the field to user profile editing screen.
 add_action(
     'edit_user_profile',
-    'wporg_usermeta_form_field_birthday'
+    'wporg_usermeta_form_field_birthdate'
 );
   
 // Add the save action to user's own profile editing screen update.
 add_action(
     'personal_options_update',
-    'wporg_usermeta_form_field_birthday_update'
+    'wporg_usermeta_form_field_birthdate_update'
 );
   
 // Add the save action to user profile editing screen update.
 add_action(
     'edit_user_profile_update',
-    'wporg_usermeta_form_field_birthday_update'
+    'wporg_usermeta_form_field_birthdate_update'
 );
 
 /* FIM DO CAMPO DE BIRTHDATE*/
