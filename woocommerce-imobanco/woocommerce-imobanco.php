0724b5d6d5c04ccfb245b9ff5fcb8f3420d1ba65
Mateus <mateus_sat@hotmail.com>
2020-07-15 10:35:30 -0300

    Identa e faz o if para verificar o imopay_id

--- woocommerce-imobanco/woocommerce-imobanco.php
+++ woocommerce-imobanco/woocommerce-imobanco.php
@@ -15,69 +15,95 @@ License: GPLv2 or later
 /*Trecho do codigo para DEBUG*/
 
 /** Step 1. */
-//   function my_plugin_menu() {
-//   	add_options_page( 'My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-//   }
+function my_plugin_menu()
+{
+    add_options_page('My Plugin Options', 'Requisição', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
+}
 
 /** Step 2 (from text above). */
-//   add_action( 'admin_menu', 'my_plugin_menu' );
+add_action('admin_menu', 'my_plugin_menu');
 
 /** Step 3. */
-//   function my_plugin_options() {
-//   	if ( !current_user_can( 'manage_options' ) )  {
-//          wp_die( __( 'Você não tem permissão suficiente para acessar essa pagina' ) );        
-//   	}
+function my_plugin_options()
+{
+    if (!current_user_can('manage_options')) {
+        wp_die(__('Você não tem permissão suficiente para acessar essa pagina'));
+    }
 
-//       $url = 'http://django:8000/buyers/';
-      	 
-//       $current_user_id = get_current_user_id();
-//       $user_meta = get_user_meta($current_user_id);
-//       $user_data = get_userdata($current_user_id);
-            
-//     //   $user_data_email = $user_data->data->user_email; // forma de acessar os attrs quando for stdclass
+    $url = 'http://django:8000/buyers/';
 
-//       $bday = $user_meta['birthday']['0']; // forma de acessar os attr quando for array
-//       $cpf_cnpj = $user_meta['cpfcnpj']['0'];
-//       $email = $user_data->data->user_email;
-//       $firstName = $user_meta['first_name']['0'];
-//       $lastName = $user_meta['last_name']['0'];
-//     //  $phone = 
-      
-//  	 $response = wp_remote_post($url,$args = [
+    $current_user_id = get_current_user_id();
+    $user_meta = get_user_meta($current_user_id);
+    $user_data = get_userdata($current_user_id);
 
-//  	 	'headers' =>[
-// 	 		'Content-Type' => 'application/json',
-//  	 		'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
-//  	 		]
-//  	 	,
-//  	 	'body' => json_encode([ 
-//  	 		'birthdate' => $bday,
-//  	 		'cpf_cnpj' => $cpf_cnpj,
-//  	 		'email' => $email,
-//             'first_name' => $firstName,
-//             'last_name' => $lastName,
-//             'mobile_phone' => '84999999999'
- 	 				
-// 	 	])
-//  	 ]
-			
-//       );         
-      
-      
+    //     //   $user_data_email = $user_data->data->user_email; // forma de acessar os attrs quando for stdclass
 
-//  	  $http_code = wp_remote_retrieve_response_code( $response );		
-//        $body = json_decode($response['body'],true);
-       
-//        return update_user_meta(
-//         $current_user_id,
-//         'imopay_id',
-//         $body['id']
-//     );
+    $bday = $user_meta['birthdate']['0']; // forma de acessar os attr quando for array
+    $cpf_cnpj = $user_meta['cpfcnpj']['0'];
+    $email = $user_data->data->user_email;
+    $firstName = $user_meta['first_name']['0'];
+    $lastName = $user_meta['last_name']['0'];
+    //  $phone = 
+    $imopay_id = $user_meta['imopay_id']['0'];
 
-//  	   echo '<pre>';
-//  	   print_r($body);
-//  	   echo '</pre>';
-//   }
+    if ($user_meta['imopay_id']['0'] != null) {
+        //fazer PUT
+        $url_put = "http://django:8000/buyers/$imopay_id/";
+
+        $response = wp_remote_post($url_put, $args = [
+            'method'      => 'PATCH',
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
+            ],
+            'body' => json_encode([
+                'birthdate' => $bday,
+                'cpf_cnpj' => $cpf_cnpj,
+                'email' => $email,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'mobile_phone' => '84999999999'
+
+            ])
+        ]);
+    } else {
+        // fazer o POST
+
+        $response = wp_remote_post($url, $args = [
+
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
+            ],
+            'body' => json_encode([
+                'birthdate' => $bday,
+                'cpf_cnpj' => $cpf_cnpj,
+                'email' => $email,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'mobile_phone' => '84999999999'
+
+            ])
+        ]);
+    } // fim do else     
+
+
+
+    $http_code = wp_remote_retrieve_response_code($response);
+    $body = json_decode($response['body'], true);
+
+    if ($http_code == 200 || $http_code == 201) {
+        return update_user_meta(
+            $user_id,
+            'imopay_id',
+            $body['id']
+        );
+    } else {
+        echo '<pre>';
+        print_r("Erro na Requisição - Status Code: $http_code");
+        echo '</pre>';
+    }
+}
 
 // essa função cria um paragrafo na tela principal do WP
 /*function ola_plugin(){	
@@ -92,15 +118,16 @@ License: GPLv2 or later
 
 /* Trecho de codigo sem DEBUG */
 
-add_action( 'profile_update', 'chama_api', 10, 2 );
+add_action('profile_update', 'chama_api', 10, 2);
 
-function chama_api($user_id) {
-	
-	$url = 'http://django:8000/buyers/';
-   
+function chama_api($user_id)
+{
+
+    $url = 'http://django:8000/buyers/';
+
     $user_meta = get_user_meta($user_id);
     $user_data = get_userdata($user_id);
-    
+
     $bday = $user_meta['birthdate']['0'];
     $cpf_cnpj = $user_meta['cpfcnpj']['0'];
     $email = $user_data->data->user_email;
@@ -108,35 +135,37 @@ function chama_api($user_id) {
     $lastName = $user_meta['last_name']['0'];
     $phone = $user_meta['phone']['0'];
 
-	$response = wp_remote_post($url,$args = [
+    $response = wp_remote_post($url, $args = [
 
-        'headers' =>[
-           'Content-Type' => 'application/json',
+        'headers' => [
+            'Content-Type' => 'application/json',
             'Authorization' => 'Api-Key 2MHFG1yr.t0t2243G9nSSuOqM90JkbA4Ndx9JwmCK'
-            ]
-        ,
-        'body' => json_encode([ 
-          'birthdate' => $bday,
-          'cpf_cnpj' => $cpf_cnpj,
-          'email' => $email,
-          'first_name' => $firstName,
-          'last_name' => $lastName,
-          'mobile_phone' => $phone
-                    
-       ])
-    ]
-			
-	);
-	
-	$http_code = wp_remote_retrieve_response_code( $response );		
-	$body = json_decode($response['body'],true);	
+        ],
+        'body' => json_encode([
+            'birthdate' => $bday,
+            'cpf_cnpj' => $cpf_cnpj,
+            'email' => $email,
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'mobile_phone' => $phone
 
-	return update_user_meta(
-        $user_id,
-        'imopay_id',
-        $body['id']
-    );
+        ])
+    ]);
 
+    $http_code = wp_remote_retrieve_response_code($response);
+    $body = json_decode($response['body'], true);
+
+    if ($http_code == 200 || $http_code == 201) {
+        return update_user_meta(
+            $user_id,
+            'imopay_id',
+            $body['id']
+        );
+    } else {
+        echo '<pre>';
+        print_r("Erro na Requisição: $http_code");
+        echo '</pre>';
+    }
 }
 
 /* INICIO DO CAMPO DE BIRTHDATE*/
@@ -145,8 +174,9 @@ function chama_api($user_id) {
  *
  * @param $user WP_User user object
  */
-function wporg_usermeta_form_field_birthdate( $user ){
-	?>
+function wporg_usermeta_form_field_birthdate($user)
+{
+?>
     <h3>Birthdate</h3>
     <table class="form-table">
         <tr>
@@ -154,21 +184,14 @@ function wporg_usermeta_form_field_birthdate( $user ){
                 <label for="birthdate">Birthdate</label>
             </th>
             <td>
-                <input type="date"
-                       class="regular-text ltr"
-                       id="birthdate"
-                       name="birthdate"
-                       value="<?= esc_attr( get_user_meta( $user->ID, 'birthdate', true ) ) ?>"
-                       title="Please use YYYY-MM-DD as the date format."
-                       pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])"
-                       required>
+                <input type="date" class="regular-text ltr" id="birthdate" name="birthdate" value="<?= esc_attr(get_user_meta($user->ID, 'birthdate', true)) ?>" title="Please use YYYY-MM-DD as the date format." pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])" required>
                 <p class="description">
                     Please enter your birthdate.
                 </p>
             </td>
         </tr>
     </table>
-    <?php
+<?php
 }
 
 /**
@@ -179,13 +202,13 @@ function wporg_usermeta_form_field_birthdate( $user ){
  * @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
  */
 
-function wporg_usermeta_form_field_birthdate_update( $user_id )
+function wporg_usermeta_form_field_birthdate_update($user_id)
 {
     // check that the current user have the capability to edit the $user_id
-    if ( ! current_user_can( 'edit_user', $user_id ) ) {
+    if (!current_user_can('edit_user', $user_id)) {
         return false;
     }
-  
+
     // create/update user meta for the $user_id
     return update_user_meta(
         $user_id,
@@ -199,19 +222,19 @@ add_action(
     'show_user_profile',
     'wporg_usermeta_form_field_birthdate'
 );
-  
+
 // Add the field to user profile editing screen.
 add_action(
     'edit_user_profile',
     'wporg_usermeta_form_field_birthdate'
 );
-  
+
 // Add the save action to user's own profile editing screen update.
 add_action(
     'personal_options_update',
     'wporg_usermeta_form_field_birthdate_update'
 );
-  
+
 // Add the save action to user profile editing screen update.
 add_action(
     'edit_user_profile_update',
@@ -227,9 +250,9 @@ add_action(
  * @param $user WP_User user object
  */
 
-function wporg_usermeta_form_field_cpf_cnpj( $user )
+function wporg_usermeta_form_field_cpf_cnpj($user)
 {
-    ?>
+?>
     <h3>It's Your CPF/CNPJ</h3>
     <table class="form-table">
         <tr>
@@ -237,20 +260,14 @@ function wporg_usermeta_form_field_cpf_cnpj( $user )
                 <label for="cpfcnpj">CPF/CNPJ</label>
             </th>
             <td>
-                <input type="number"
-                       class="regular-text ltr"
-                       id="cpfcnpj"
-                       name="cpfcnpj"
-                       value="<?= esc_attr( get_user_meta( $user->ID, 'cpfcnpj', true ) ) ?>"
-                       title="Please use only numbers."                       
-                       required>
+                <input type="number" class="regular-text ltr" id="cpfcnpj" name="cpfcnpj" value="<?= esc_attr(get_user_meta($user->ID, 'cpfcnpj', true)) ?>" title="Please use only numbers." required>
                 <p class="description">
                     Please enter your CPF/CNPJ without dot or slash.
                 </p>
             </td>
         </tr>
     </table>
-    <?php
+<?php
 }
 
 /**
@@ -261,13 +278,13 @@ function wporg_usermeta_form_field_cpf_cnpj( $user )
  * @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
  */
 
-function wporg_usermeta_form_field_cpf_cnpj_update( $user_id )
+function wporg_usermeta_form_field_cpf_cnpj_update($user_id)
 {
     // check that the current user have the capability to edit the $user_id
-    if ( ! current_user_can( 'edit_user', $user_id ) ) {
+    if (!current_user_can('edit_user', $user_id)) {
         return false;
     }
-  
+
     // create/update user meta for the $user_id
     return update_user_meta(
         $user_id,
@@ -281,19 +298,19 @@ add_action(
     'show_user_profile',
     'wporg_usermeta_form_field_cpf_cnpj'
 );
-  
+
 // Add the field to user profile editing screen.
 add_action(
     'edit_user_profile',
     'wporg_usermeta_form_field_cpf_cnpj'
 );
-  
+
 // Add the save action to user's own profile editing screen update.
 add_action(
     'personal_options_update',
     'wporg_usermeta_form_field_cpf_cnpj_update'
 );
-  
+
 // Add the save action to user profile editing screen update.
 add_action(
     'edit_user_profile_update',
@@ -309,9 +326,9 @@ add_action(
  * @param $user WP_User user object
  */
 
-function wporg_usermeta_form_field_phone( $user )
+function wporg_usermeta_form_field_phone($user)
 {
-    ?>
+?>
     <h3>It's Your Phone</h3>
     <table class="form-table">
         <tr>
@@ -319,20 +336,14 @@ function wporg_usermeta_form_field_phone( $user )
                 <label for="phone">Phone Number</label>
             </th>
             <td>
-                <input type="number"
-                       class="regular-text ltr"
-                       id="phone"
-                       name="phone"
-                       value="<?= esc_attr( get_user_meta( $user->ID, 'phone', true ) ) ?>"
-                       title="Please use only numbers."                       
-                       required>
+                <input type="number" class="regular-text ltr" id="phone" name="phone" value="<?= esc_attr(get_user_meta($user->ID, 'phone', true)) ?>" title="Please use only numbers." required>
                 <p class="description">
                     Please enter your phone without any caracter special.
                 </p>
             </td>
         </tr>
     </table>
-    <?php
+<?php
 }
 
 /**
@@ -343,13 +354,13 @@ function wporg_usermeta_form_field_phone( $user )
  * @return bool Meta ID if the key didn't exist, true on successful update, false on failure.
  */
 
-function wporg_usermeta_form_field_phone_update( $user_id )
+function wporg_usermeta_form_field_phone_update($user_id)
 {
     // check that the current user have the capability to edit the $user_id
-    if ( ! current_user_can( 'edit_user', $user_id ) ) {
+    if (!current_user_can('edit_user', $user_id)) {
         return false;
     }
-  
+
     // create/update user meta for the $user_id
     return update_user_meta(
         $user_id,
@@ -363,13 +374,13 @@ add_action(
     'show_user_profile',
     'wporg_usermeta_form_field_phone'
 );
-  
+
 // Add the field to user profile editing screen.
 add_action(
     'edit_user_profile',
     'wporg_usermeta_form_field_phone'
 );
-  
+
 // Add the save action to user's own profile editing screen update.
 add_action(
     'personal_options_update',
